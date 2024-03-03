929d3df391f6cfa611cac4b50f2035c6b9d67596
j.bektemirov <b.o.jakhongir@gmail.com>
2024-03-03 12:02:58 +0500

    set up Travis configuration

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Hi there!
-      </header>
+      <header className="App-header">Hi there! We set up the Travis CI</header>
     </div>
   );
 }
